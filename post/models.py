274f5e8519d84ef06e94238e23dfeac09a05371d
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-06-11 23:15:53 +0300

    add models and migrations

--- post/models.py
+++ post/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Meta:
+        ordering = ["username", ]
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             related_name="comments")
+    comment_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.created_at}"
