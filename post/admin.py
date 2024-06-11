ddbb3037d7a9df6fb5db01fd00c2764b0c2a118f
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-06-11 23:31:52 +0300

    add models to admin panel

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from post.models import Comment, User
+
+admin.site.register(Comment)
+admin.site.register(User, UserAdmin)
