2ff5e1a47fddf6c455de9baeb8d7cebe80f6acb1
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-06-13 00:09:49 +0300

    update admin

--- post/admin.py
+++ post/admin.py
@@ -3,5 +3,10 @@ from django.contrib.auth.admin import UserAdmin
 
 from post.models import Comment, User
 
-admin.site.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["user", "comment_text", "created_at", ]
+
+
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(User, UserAdmin)
